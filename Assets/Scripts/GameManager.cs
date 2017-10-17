2785557b585377b5c382186c3027220c06b48b72
LowBudgetTech <kevinneilson6@gmail.com>
2017-10-16 23:07:35 -0400

    Added Level 1 with ghost and coin animations

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    private float coinCount;
+    private float total;
+    private float currentCount;
+
+    public Text score;
+	// Use this for initialization
+	void Start () {
+
+        coinCount = GameObject.FindGameObjectsWithTag("coin").Length;
+        total = coinCount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        coinCount = GameObject.FindGameObjectsWithTag("coin").Length;
+        currentCount = total - coinCount;
+        score.text = "Coins: " + currentCount + "/" + total;
+
+        if(coinCount <= 0)
+        {
+            Application.LoadLevel("win");
+        }
+	}
+    
+}
