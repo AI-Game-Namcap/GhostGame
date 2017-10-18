574f44a5153574b3a6afd9aef27e92704bf20352
LowBudgetTech <kevinneilson6@gmail.com>
2017-10-18 00:44:05 -0400

    Added start and game over screens

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartScreen()
+    {
+        SceneManager.LoadScene("Start Screen", LoadSceneMode.Single);
+    }
+}
