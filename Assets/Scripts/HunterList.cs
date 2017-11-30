5fd48bf8f849a375f7d76e65e146ce520e125443
Unknown <attila.godspeed@gmail.com>
2017-11-30 12:58:00 -0500

    Implemented Hunters calling other Hunters, Scripts only

---
+++ Assets/Scripts/HunterList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterList : MonoBehaviour {
+
+	[SerializeField] private Hunter[] hunters;
+
+	// Use this for initialization
+	void Start () {	}
+
+	// Method to send call-out to all the hunters in the list
+	public void callHunters(Vector3 location) {
+		for(int i=0; i<hunters.Length; i++) {
+			if(hunters[i] != null)
+				hunters[i].callIn(location);
+		}
+	}
+}
