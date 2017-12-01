8ca713f06c1f7048263261290873fd60d429fe3c
LowBudgetTech <kevinneilson6@gmail.com>
2017-12-01 01:36:59 -0500

    Finished Game

---
+++ Assets/MusicSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSpawner : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject music;
+	void Start () {
+        Instantiate(music, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StoryMusic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryMusic : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Application.loadedLevelName == "Level1")
+        {
+            Destroy(this.gameObject);
+            
+
+        }
+        
+
+
+
+    }
+
+    public AudioClip[] audioClip;
+    AudioSource audio;
+    //static bool AudioBegin = false;
+    void Start()
+    {
+        //audio = GetComponent<AudioSource>();
+        
+            // audio.PlayOneShot(audioClip[0], 1f);
+            GetComponent<AudioSource>().Play();
+            DontDestroyOnLoad(gameObject);
+            //AudioBegin = true;
+        
+        
+
+    }
+}
